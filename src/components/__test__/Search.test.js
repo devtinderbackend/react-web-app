92b6249fab0e4e5c8e5e5834ff12077ea7240423
devtinderbackend <devtinderbackend@gmail.com>
2024-12-19 11:53:43 +0530

    Top Rated Restaurant Integration Testing

--- src/components/__test__/Search.test.js
+++ src/components/__test__/Search.test.js
@@ -1,4 +1,3 @@
-
 import { fireEvent, render, screen } from "@testing-library/react"
 import MOCK_DATA from "../mocks/restaurantListCards.json"
 import Body from "../../components/Body"
@@ -6,27 +5,45 @@ import { act } from "react"
 import { BrowserRouter } from "react-router-dom"
 import "@testing-library/jest-dom"
 
-global.fetch = jest.fn(() => {
-    return Promise.resolve(
-        {
-            json: () => {
-                return Promise.resolve(MOCK_DATA);
-            }
-        })
-})
+describe("Test Body Component", () => {
+    global.fetch = jest.fn(() => {
+        return Promise.resolve(
+            {
+                json: () => {
+                    return Promise.resolve(MOCK_DATA);
+                }
+            })
+    })
 
-it("Should render body component with search", async () => {
-    await act(async () =>
+    it("Should render body component with search", async () => {
+        await act(async () =>
 
-        render(<BrowserRouter>
-            <Body />
-        </BrowserRouter>
+            render(<BrowserRouter>
+                <Body />
+            </BrowserRouter>
+            )
         )
-    )
-    const searchBtn = screen.getByRole("button", { name: "Search" })
-    const searchInput = screen.getByTestId("searchInput")
-    fireEvent.change(searchInput, { target: { value: "Pizza" } })
-    fireEvent.click(searchBtn);
-    const cards = screen.getAllByTestId("restCard");
-    expect(cards.length).toBe(2);
-})
+        const searchBtn = screen.getByRole("button", { name: "Search" })
+        const searchInput = screen.getByTestId("searchInput")
+        fireEvent.change(searchInput, { target: { value: "Pizza" } })
+        fireEvent.click(searchBtn);
+        const cards = screen.getAllByTestId("restCard");
+        expect(cards.length).toBe(2);
+    })
+
+    it("Should render body component with Top Rated Restaurant", async () => {
+        await act(async () =>
+
+            render(<BrowserRouter>
+                <Body />
+            </BrowserRouter>
+            )
+        )
+        const cardsBeforeFilter = screen.getAllByTestId("restCard")
+        expect(cardsBeforeFilter.length).toBe(20);
+        const topRatedBtn = screen.getByRole("button", { name: "Top Rated Restaurant" })
+        fireEvent.click(topRatedBtn)
+        const cardAfterFilter = screen.getAllByTestId("restCard")
+        expect(cardAfterFilter.length).toBe(12);
+    })
+    })
